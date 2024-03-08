0746f2f3d5e871a792de66f77293f3137cd49966
Zamarr0n <emilio.zamarrons@gmail.com>
2024-03-08 17:11:49 -0600

    week02 - activity team

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,5 +1,10 @@
 let olastModified = new Date(document.lastModified);
 const lastModified = document.querySelector('#lastModified');
+lastModified.textContent = olastModified;
+const hamButton = document.querySelector("#menu")
+const navigation = document.querySelector(".navigation");
 
-lastModified.textContent=olastModified; 
-console.log(olastModified);
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});

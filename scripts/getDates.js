41f965b5cb41e17d34e734d85bbdbd9ce1b5f2b5
Zamarr0n <emilio.zamarrons@gmail.com>
2024-03-16 20:21:49 -0600

    team assignment week03

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,8 +3,21 @@ const lastModified = document.querySelector('#lastModified');
 lastModified.textContent = olastModified;
 const hamButton = document.querySelector("#menu")
 const navigation = document.querySelector(".navigation");
+const visits = document.getElementById("visits")
 
 hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
 });
+
+let numOfVisits = Number(window.localStorage.getItem('numOfVisit-ls'));
+
+if (numOfVisits !== 0){
+    visits.textContent = numOfVisits;
+}else{
+    visits.textContent = "Congrats you are the first visit 😁";
+}
+
+numOfVisits++;
+
+localStorage.setItem('numOfVisits-ls', visits);

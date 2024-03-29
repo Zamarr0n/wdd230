9e30e0a5338875f598c498066a8895a3035f36ba
Zamarr0n <emilio.zamarrons@gmail.com>
2024-03-28 21:40:06 -0600

    .

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -4,10 +4,8 @@ const figcaption = document.querySelector("figcaption");
 const latitude = '49.75'; 
 const longitude = '6.64';
 const api_key = 'f4020f49719cc815617a223c13df5681';
-// const url = `https://api.openweathermap.org/data/3.0/onecall?lat=49.75&lon=6.64&appid=f4020f49719cc815617a223c13df5681&units=imperial`;
 const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${api_key}&units=imperial`;
-// https://api.openweathermap.org/data/2.5/weather?lat=33.44&lon=-94.04&appid=6c92f2dd8b37c36db2caf23f66bfdf59
-// API-ID: 81bdd07d2a0c406792f9d519ff0ecb70
+
 async function apiFetch(){
     try{
         const response = await fetch(url);

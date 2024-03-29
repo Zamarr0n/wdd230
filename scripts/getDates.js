8b498338de9aea9617343cc68bb456f7bc975119
Zamarr0n <emilio.zamarrons@gmail.com>
2024-03-28 23:37:24 -0600

    team assignment week05

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -7,8 +7,8 @@ var month = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
 var year = date.getFullYear();
 date = month + '/' + day + '/' + year;
 lastModified.textContent = date;
-lastStamp.textContent = date
-// Today´s Date
+// lastStamp.textContent = date;
+
 
 // navbar Code
 const hamButton = document.querySelector("#menu")
@@ -20,6 +20,7 @@ hamButton.addEventListener('click', () => {
     hamButton.classList.toggle('open');
 });
 
+//LocalStorage for the visits:
 let numOfVisits = Number(localStorage.getItem('Number of visits:'));
 
 if (numOfVisits !== 0){
@@ -31,3 +32,34 @@ if (numOfVisits !== 0){
 numOfVisits++;
 
 localStorage.setItem('Number of visits:', numOfVisits);
+
+//API constants and information :
+const api_key = 'f4020f49719cc815617a223c13df5681';
+const weather_div = document.querySelector("#weather_info"); 
+const icon = document.querySelector("#icon-weather");
+const temperature = document.querySelector("#temperature");
+const description = document.querySelector("#description-weather");
+
+async function fetchData(){
+    var cityname = document.querySelector("#city").value
+    if(cityname !==''){
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${api_key}&units=imperial`;
+    const response = await fetch(url);
+    if(response.ok){
+        const data = await response.json();
+        displayData(data);
+    }else{
+        console.log("error");
+    }
+}
+}
+function displayData(data){
+    temperature.innerHTML = `${data.main.temp} C`;
+    const icon_url =`https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description
+    icon.setAttribute("src",icon_url);
+    icon.setAttribute("alt", desc);
+    description.innerHTML = `${desc}`;
+}
+
+document.querySelector("#getData").addEventListener('click', fetchData);
--- scripts/links.js
+++ scripts/links.js
@@ -5,7 +5,7 @@ const linksURL = "https://Zamarr0n.github.io/wdd230/data/links.json"
 async function getLinks(){
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    displayLinks(data);
 }
 getLinks();
 

149b18ed38fb72323aaf3131413c16333de62c81
Zamarr0n <emilio.zamarrons@gmail.com>
2024-03-29 22:43:12 -0600

    .

--- chamber/script/directory.js
+++ chamber/script/directory.js
@@ -1,8 +1,21 @@
-const div = documet.querySelect("#cards-div");
-const URL = "https://Zamarr0n.github.io/wdd230/chamber/data/members.json"
+// const div = documet.querySelect("#cards-div");
+const url = "https://Zamarr0n.github.io/wdd230/chamber/data/members.json";
 
+async function getLink(){
+    const respond = await fetch(url);
+    const data = await respond.json();
+    console.log(data);
+    displayData(data.info);
+}
 
+const displayData = (data) => {
+    data.forEach(element => {
+        const name = element.name;
+        console.log(name)
+    });
+}
 
+getLink();
 
 
 
